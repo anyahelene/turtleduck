7de7083d8ff3837cfd4536f237f96bc185205798
Anya Helene Bagge <anya@ii.uib.no>
2023-02-23 12:00:47 +0100

    moved to external project

--- borb/src/IO.ts
+++
@@ -1,4 +0,0 @@
-interface Printer {
-    print(...args: string[]): string;
-    println(...args: string[]): string;
-}
--- borb/src/remarkable.js
+++
@@ -1,3 +0,0 @@
-import { Remarkable, utils } from "remarkable";
-
-export { Remarkable, utils};
